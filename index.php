2612e3b44aec8128bc4b2f2f33ff541a44b381b0
AkilAkil <agilan.madhiyan@gmail.com>
2017-10-04 14:45:10 +0530

    init

--- index.php
+++ index.php
@@ -105,28 +105,23 @@ Steve & Jacquelyn, Bundeena NSW</h5>
   <footer class="page-footer teal">
     <div class="container">
       <div class="row">
-        <div class="col l6 s12">
-          <h5 class="white-text">Company Bio</h5>
-          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+        <div class="col l9 s12">
+          <h5 class="white-text">Hotel History</h5>
+          <p class="grey-text text-lighten-4">The Paradise restaurant, founded in 1953, began as a small cafe serving tea and snacks, but now it serves traditional Hyderabadi, Indian and Chinese continental delicacies. The hotel is known for Hyderabadi dishes.</p>
 
 
         </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">Settings</h5>
-          <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
-          </ul>
-        </div>
+        
         <div class="col l3 s12">
           <h5 class="white-text">Connect</h5>
           <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
+            <ul>
+            <li><a class="white-text" href="https://www.facebook.com/Paradise"><i class="material-icons prefix">facebook</i>Facebook</a></li>
+            <li><a class="white-text" href="https://www.gmail.com"><i class="material-icons prefix">email</i>paradise@hotel.com</a></li>
+            <li><a class="white-text" ><i class="material-icons prefix">phone</i>0141-6700700</a></li>
+            <li><a class="white-text" ><i class="material-icons prefix">chat</i>9685741458</a></li>
+            
+          </ul>
           </ul>
         </div>
       </div>
--- onlinebook.php
+++ onlinebook.php
@@ -1,4 +1,8 @@
 <html>
+<?php
+ob_start();
+session_start();
+?>
 <head>
 
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
@@ -45,8 +49,7 @@
       <ul class="right hide-on-med-and-down">
         <li><a href="index.php">Home</a></li>
       
-        <li><a href="onlinebook.php">Book Online</a></li>
-      
+        
          
         <?php
         if(isset($_SESSION["uname"]) && $_SESSION["uname"] == "admin")
@@ -139,28 +142,20 @@ else
  <footer class="page-footer teal" >
     <div class="container" >
       <div class="row">
-        <div class="col l6 s12">
-          <h5 class="white-text">Company Bio</h5>
+        <div class="col l9 s12">
+          <h5 class="white-text">Hotel History</h5>
           <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
 
 
         </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">Settings</h5>
-          <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
-          </ul>
-        </div>
+       
         <div class="col l3 s12">
           <h5 class="white-text">Connect</h5>
           <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
+            <li><a class="white-text" href="https://www.facebook.com/Paradise"><i class="material-icons prefix">facebook</i>Facebook</a></li>
+            <li><a class="white-text" href="https://www.gmail.com"><i class="material-icons prefix">email</i>paradise@hotel.com</a></li>
+            <li><a class="white-text" ><i class="material-icons prefix">phone</i>0141-6700700</a></li>
+            <li><a class="white-text" ><i class="material-icons prefix">chat</i>9685741458</a></li>
           </ul>
         </div>
       </div>
